e5eee27bddfb473cc347dd84f7d93a75cce6cd1b
ульба <ulbwa@icloud.com>
2024-11-04 21:46:37 +0500

    fix module name

--- metmuseum/types/models/__init__.py
+++ metmuseum/types/models/__init__.py
@@ -2,7 +2,7 @@ from metmuseum.types.models.department_element import DepartmentElement
 from metmuseum.types.models.object_constituent import ObjectConstituent
 from metmuseum.types.models.object_dimensions import ObjectDimensions
 from metmuseum.types.models.object_measurements import ObjectMeasurements
-from metmuseum.types.models.object_measurementsElement import ObjectMeasurementsElement
+from metmuseum.types.models.object_measurements_element import ObjectMeasurementsElement
 from metmuseum.types.models.object_tag import ObjectTag
 
 __all__ = (
